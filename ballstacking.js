1d100659d9839f7381e73d3eeff29097fbc28d88
“Patrick <pctennislessons@gmail.com>
2019-11-03 20:44:03 -0700

    finished

--- ballstacking.js
+++ ballstacking.js
@@ -1,6 +1,5 @@
 function stackHeight3d(layers) {
     // take the hight of one ball 
     // figure out the how low a ball is going to have to sit on a ball and subtact that amount from the hight
-
-    return 0.0
+    return  layers / Math.SQRT2
 }
---
+++ phoneNumber.js
@@ -0,0 +1,11 @@
+function createPhoneNumber(numbers){
+    return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6,10).join('')}`
+  }
+  
+  function createPhoneNumber(numbers){
+      let numString = numbers.join('')
+      return `(${numString.substring(0,3)}) ${numString.substring(3,6)}-${numString.substring(6,10)}`
+      
+  }
+  
+  console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == "(123) 456-7890");
