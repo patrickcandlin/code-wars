f902c8934a9df62b8df407c267e05b0cd84274ca
“Patrick <pctennislessons@gmail.com>
2019-11-05 14:47:40 -0700

    moving on because I am stuck

---
+++ pythagoreanTriplets.js
@@ -0,0 +1,28 @@
+/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
+// a^2+b^2=c^2
+// c = sqrt(a^2 + b^2)
+// = sqrt(a^2 + b^2)
+// a < b < c
+// 1000 = a + b + sqrt(a^2 + b^2)
+// (1000 - b - a)^2 - b^2 = a2
+// 1000 = (1000(b-500))/(b-1000) + b + sqrt(((1000(b-500))/(b-1000))^2 + b^2)
+
+function findPythagoreanTripletBySum(sum){
+    //set the triplet equal to to the sum of three numbers 
+    let c = 3
+    let b = 2
+    let a = 1
+   //returns the products that equal the sum
+   if (c + b + a === sum)
+    return 
+}
+
+console.log(findPythagoreanTripletBySum(12), '345')
