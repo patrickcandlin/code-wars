3d32979fa1f8434d632fb9153ffb0734520cfbfc
“Patrick <pctennislessons@gmail.com>
2019-11-14 09:42:41 -0700

    finished ball stacking problem

--- ballstacking.js
+++ ballstacking.js
@@ -1,5 +1,4 @@
-function stackHeight3d(layers) {
-    // take the hight of one ball 
-    // figure out the how low a ball is going to have to sit on a ball and subtact that amount from the hight
-    return  layers / Math.SQRT2
+function stackHeight3d(layers, diameter=1) {
+    return  diameter + (layers - 1)*diameter*(Math.sqrt(2/3))
 }
+
